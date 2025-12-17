b09cbefe47435143697ca65208c8d03669c008b8
Pac <rafaelalves@hotmail.com>
2025-12-17 20:29:19 +0100

    Update done.sh

--- submissions/done.sh
+++ submissions/done.sh
@@ -1,6 +1,6 @@
 # When you finish the workshop:
 # 1. add a link to your replit fork in the next line
-#
+# https://replit.com/@rafaelpac/Programming-Lightning-Intro-to-Payment-Channels-mini
 #
 # 2. change the FAIL string to PASS.
-echo "FAIL"
+echo "PASS"
